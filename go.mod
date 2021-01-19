dfa30f5d06b39b6b58e58a240497002aa768258e
Arris Li <arrisli@iqhive.com>
2021-01-19 17:52:25 +1300

    add pinger

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/ArrisLee/distributor
 
 go 1.15
 
-require github.com/go-echarts/go-echarts/v2 v2.2.3
+require (
+	github.com/go-echarts/go-echarts/v2 v2.2.3
+	github.com/go-ping/ping v0.0.0-20201115131931-3300c582a663
+)
--- main.go
+++ main.go
@@ -1,43 +1,52 @@
 package main
 
 import (
-	"math/rand"
+	"log"
 	"net/http"
-	"strconv"
+	"time"
 
 	"github.com/go-echarts/go-echarts/v2/charts"
 	"github.com/go-echarts/go-echarts/v2/opts"
 	"github.com/go-echarts/go-echarts/v2/types"
+	"github.com/go-ping/ping"
 )
 
-// generate random data for line chart
-func generateLineItems() []opts.LineData {
-	items := make([]opts.LineData, 0)
-	for i := 0; i < 5; i++ {
-		items = append(items, opts.LineData{Name: strconv.Itoa(i + 1), Value: rand.Intn(20), Symbol: "diamond", SymbolSize: 10})
+func getPingData(url string) ([]opts.LineData, []int32, string) {
+	pinger, _ := ping.NewPinger(url)
+	pinger.Count = 10
+	pinger.Run()
+	var rttsMS []int64
+	var XAxis []int32
+	for i := range pinger.Statistics().Rtts {
+		rttsMS = append(rttsMS, int64(pinger.Statistics().Rtts[i]/time.Millisecond))
 	}
-	return items
+	var items []opts.LineData
+	for i := 0; i < len(rttsMS); i++ {
+		XAxis = append(XAxis, int32(i+1))
+		items = append(items, opts.LineData{Value: rttsMS[i], Symbol: "diamond", SymbolSize: 10})
+	}
+	return items, XAxis, pinger.Statistics().IPAddr.String()
 }
 
-func httpserver(w http.ResponseWriter, _ *http.Request) {
-	// create a new line instance
+func httpserver(w http.ResponseWriter, r *http.Request) {
+	url := r.URL.Query()["target"][0]
+	lineData, XAxis, ipInfo := getPingData(url)
+
 	line := charts.NewLine()
-	// set some global options like Title/Legend/ToolTip or anything else
 	line.SetGlobalOptions(
 		charts.WithInitializationOpts(opts.Initialization{Theme: types.ThemeWonderland}),
 		charts.WithTitleOpts(opts.Title{
-			Title:    "www.google.com",
-			Subtitle: "1.1.1.1",
+			Title:    url,
+			Subtitle: ipInfo,
 		}))
-
-	// Put data into instance
-	line.SetXAxis([]int32{1, 2, 3, 4, 5}).
-		AddSeries("Category A", generateLineItems()).
+	line.SetXAxis(XAxis).
+		AddSeries("Category A", lineData).
 		SetSeriesOptions(charts.WithLineChartOpts(opts.LineChart{Smooth: true}))
 	line.Render(w)
 }
 
 func main() {
 	http.HandleFunc("/", httpserver)
+	log.Println("start to listen on :8081")
 	http.ListenAndServe(":8081", nil)
 }
